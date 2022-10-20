32b51df5b6f57e913b4c70fdf8a6f557b20e5234
Dave <48969490+sugarvoid@users.noreply.github.com>
2022-10-20 08:14:30 -0500

    Added meteor wobble/pulse effect

--- game/manager/meteor_manager.gd
+++ game/manager/meteor_manager.gd
@@ -1,16 +1,42 @@
 class_name  MeteorManager
 extends Node2D
 
+@onready var inner_node: Node2D = get_node("Inner")
+@onready var outer_node: Node2D = get_node("Outer")
+
 const p_Meteor: PackedScene = preload("res://game/meteor/meteor.tscn")
+
+var inner_positions: Array
+var outer_positions: Array
 
 func add_metor_to_screen(pos: Vector2) -> void:
 	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_resize_arrays()
+	_add_pos_to_inner_array()
+	_add_pos_to_outer_array()
+	print(self.inner_positions)
+	print(self.outer_positions)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _resize_arrays() -> void:
+	self.inner_positions.resize(9)
+	self.outer_positions.resize(9)
+
+func _add_pos_to_inner_array() -> void:
+	var i = 0
+	for pos in self.inner_node.get_children():
+		self.inner_positions[i] = pos.global_position
+		i += 1
+		
+func _add_pos_to_outer_array() -> void:
+	var i = 0
+	for pos in self.outer_node.get_children():
+		self.outer_positions[i] = pos.global_position
+		i += 1
--- game/meteor/meteor.gd
+++ game/meteor/meteor.gd
@@ -3,6 +3,8 @@ extends Node2D
 
 @onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
 
+const WOBBLE_SIZE: Vector2 = Vector2(0.1,0.1)
+
 enum TYPES {
 	NUMBER,
 	COLOR,
@@ -15,11 +17,16 @@ var value: String
 
 func _ready() -> void:
 	self.type = Meteor.TYPES.NUMBER
+	self._wobble()
 
+func _process(delta: float) -> void:
+	print(self.get_current_scale())
 
 func check_for_match(input: String) -> bool:
 	return true
 
+func get_current_scale() -> Vector2:
+	return self.scale
 
 func flip_over() -> void:
 	self.animation_player.play("flip_over")
@@ -37,7 +44,17 @@ func flip_over() -> void:
 	
 	self.animation_player.play_backwards("flip_over")
 	await  self.animation_player.animation_finished
+	
+
+func _wobble() -> void:
+	var tween: Tween = create_tween() 
+	tween.tween_property(self, "scale", (self.get_current_scale() - self.WOBBLE_SIZE), 1.0)
+	await  tween.finished
+
+	var tween2: Tween = create_tween()
+	tween2.tween_property(self, "scale", (self.get_current_scale() + self.WOBBLE_SIZE), 1.0)
+	await  tween2.finished
+
+	self._wobble()
 
 
-func _on_timer_timeout() -> void:
-	flip_over()
