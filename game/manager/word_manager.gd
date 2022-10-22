dc948cdc2bb973a5d7c4427d49617c8bb80d9453
Dave <48969490+sugarvoid@users.noreply.github.com>
2022-10-22 16:14:20 -0500

    Changed screen size, background sprite

--- game/manager/word_manager.gd
+++ game/manager/word_manager.gd
@@ -1,9 +1,11 @@
 class_name WordManager
 extends Node
 
+@onready var word_display: Label = get_node("WordDisplay")
 
 const LETTERS: Array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
+var typed_word: Array
 
 var test_var = "test string"
 
@@ -23,3 +25,12 @@ func get_letter_vaule(letter: String) -> int:
 			return 8
 		_:
 			return -99
+
+func add_letter(letter: String) -> void:
+	pass
+
+func _update_word_label() -> void:
+	pass
+	
+func _letter_array_to_string() -> String:
+	return "filter"
